97b0789b5ac80e9492215797522cb90ae7503f73
Bob Adolf <rdadolf@gmail.com>
2015-02-20 17:31:46 -0500

    Added a prime number library.

---
+++ solutions/primes.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+class Sieve():
+  def __init__(self,upto=100):
+    self._upto = upto
+    self._size = 3
+    self._memo = np.zeros(self._size,dtype='int64')+1
+    self._memo[0] = 0
+    self._memo[1] = 0
+    self._memo[2] = 1
+    self._last_factor = 2
+
+    self.gen(upto)
+
+  def _alloc(self,upto):
+    diff = upto-self._size
+    self._memo = np.concatenate((self._memo, np.zeros(diff,dtype='int64')+1))
+    self._size = upto
+
+  def gen(self,upto):
+    self._alloc(upto)
+    #print 'Sieving factors from %d to %d'%(self._last_factor,upto//2)
+    for f in range(2, upto//2):
+      # find next prime
+      if self._memo[f]:
+        #print 'Elimintating %d to %d by %d'%(f*2,self._size,f)
+        self._memo[np.arange(f*2,self._size,f)] = 0
+        self._last_factor = f
+    return self._last_factor
+
+  def primes(self):
+    return np.nonzero(self._memo)[0]
+
+  def is_prime(self,n):
+    if n<self._size:
+      self.gen(n+1)
+    return self._memo[n]
