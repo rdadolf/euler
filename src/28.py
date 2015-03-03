873d0afc8bb4fb381551d0978fc28a1eebbd3846
Bob Adolf <rdadolf@gmail.com>
2015-03-03 08:59:43 -0500

    Solved 28.

---
+++ src/28.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import numpy as np
+import util
+
+max_side = 1001
+
+# Strategy:
+#   There are four corners for each expanding box, each at a fixed interval.
+#   So just add the side length of each box four times to get the corners.
+#   Then accumulate them.
+
+n = 1
+sum = n
+for side in range(3,max_side+1,2):
+  for corner in ['bottom_right','bottom_left','top_left','top_right']:
+    n += side-1
+    sum += n
+
+print sum
