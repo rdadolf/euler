0c90bc1609f3a8c08e87389f4df10108d1c099c2
Bob Adolf <rdadolf@gmail.com>
2015-03-03 09:11:43 -0500

    Fixed naming issue in 28. Not a bug, just ugly.

--- src/28.py
+++ src/28.py
@@ -11,10 +11,10 @@ max_side = 1001
 #   Then accumulate them.
 
 n = 1
-sum = n
+accum = n
 for side in range(3,max_side+1,2):
   for corner in ['bottom_right','bottom_left','top_left','top_right']:
     n += side-1
-    sum += n
+    accum += n
 
-print sum
+print accum
